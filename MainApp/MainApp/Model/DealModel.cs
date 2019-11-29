8018ddb43da19bd078610358175a65f2dcc1fa91
Askar <ascar1@yandex.ru>
2019-11-29 16:44:28 +0500

    29112019

--- MainApp/MainApp/Model/DealModel.cs
+++ MainApp/MainApp/Model/DealModel.cs
@@ -10,12 +10,24 @@ namespace MainApp.Model
     {
         public string Tiker { get; set; }
         public string Type { get; set; }
-        public int Vol { get; set; }
+        public DateTime OpenDate { get; set; }       
         public double OpenPrice { get; set; }
         public double StopPrice { get; set; }
-        public DateTime OpenDate { get; set; }
+        public int Vol { get; set; }
         public double ClosePrice { get; set; }
         public DateTime CloseDate { get; set; }
         public bool InMarket { get; set; }
+        public List<HistoryDeal> History { get; set; }
     }
+
+    public class HistoryDeal
+    {
+        public DateTime Date { get; set; }
+        public Double Open { get; set; }
+        public Double High { get; set; }
+        public Double Low { get; set; }
+        public Double Close { get; set; }
+        public double StopPrice { get; set; }
+    }
+
 }
